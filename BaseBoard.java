72d4d3b99d5bb1c113425899a4cbd165075b793f
dottidee <38963734+dottidee@users.noreply.github.com>
2018-11-27 13:29:09 -0500

    Update BaseBoard.java

--- BaseBoard.java
+++ BaseBoard.java
@@ -16,5 +16,30 @@ public class BaseBoard {
 					{0,0,7,0,8,3,0,6,9}};
 		return board;
 	}
-	
+	//add 2d array for boxes
+	int [][] boxes = new int[3][3];
+
+	private boolean inRow(int row, int x)
+	{
+		for(int i = 0; i < column; i ++){
+		if(board[row][i] == x)
+			return true;
+
+		else
+			return false;
+	}
+	}
+	private boolean inColumn(int column, int x)
+	{	
+		for(int i = 0; i < row; i ++){
+			if(board[i][column] == x)
+				return true;
+			else
+				return false;
+		}
+	}
+	private boolean inBox(int row, int column, int x)
+	{
+
+	}
 }
