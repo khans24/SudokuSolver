4328ba789ab687ba514750cd0ffde8cc40b06201
khans24 <45401400+khans24@users.noreply.github.com>
2018-11-27 14:56:53 -0500

    Update BaseBoard.java

--- BaseBoard.java
+++ BaseBoard.java
@@ -2,7 +2,7 @@ package Solver;
 
 public class BaseBoard {	
 	public static void main(String[] args) {
-	public static int board[][] = new int[][]{
+	int board[][] = new int[][]{
 	{5,8,0,1,7,0,3,0,0},
 	{0,0,9,0,0,8,0,1,0},
 	{0,0,3,0,0,0,0,0,8},
@@ -12,57 +12,42 @@ public class BaseBoard {
 	{3,0,0,0,0,0,5,0,0},
 	{0,6,0,5,0,0,2,0,0},
 	{0,0,7,0,8,3,0,6,9}};
-	int column, row;
+	//int column, row= 8;
 
-	check();
+	//we checked inRow, inColumn, inBox methods. they work.
 	}
-	private static void check(int board[][], int row, int column, int x)
+	private static boolean inRow(int board[][], int row, int x)
 	{
-		if(inRow(board,0,5)&& inColumn(board,1,8)&& inBox(board,0,0,9)) 
-			System.out.println("works");
-	}
-	private static boolean inRow(int board[][], int row, int column, int x)
-	{
-		boolean b=true;
-		for(int i = 0; i < column; i++){
-		if(board[row][i] == x)
-			b= true;
-
-		else
-			b= false;
+		for(int i = 0;i <= 8; i++){
+			if(board[row][i] == x)
+				return true;
 		}
-		return b;
+		return false;
 	}
-	private boolean inColumn(int board[][], int row, int column, int x)
-	{	
-		boolean b=true;
-		for(int i = 0; i < row; i ++){
+	private static boolean inColumn(int board[][], int column, int x)
+	{
+		for(int i = 0; i <= 8; i ++){
 			if(board[i][column] == x)
-				b= true;
-			else
-				b= false;
+				return true;
 		}
-		return b;
+		return false;
 	}
-	private boolean inBox(int board[][], int row, int column, int x)
+	private static boolean inBox(int board[][], int row, int column, int x)
 	{
-		boolean b=true;
-		int row2 =(row+1) / 3;
-		int column2 = (column +1)/3;
+		int row2 =(row / 3) *3;
+		int column2 = (column/3)*3;
 		for(int i=row2; i<row2+3;i++){
 			for(int j=column2;j<column2+3;j++){
-				if(board[i][j] == x){
-					b= true;
-				}
-				else 
-					b= false;
+				if(board[i][j] == x)
+					return true;
 			}
-		} return b;
+		}
+		return false;
 	}
-	private boolean good(int b[][], int r, int c, int x){
-		if(!inBox(b,r,c,x) && !inColumn(b,r,c,x) && !inRow(b,r,c,x))
-			return true;
-		else 
-			return false;
-	}
+//	private boolean good(int b[][], int r, int c, int x){
+//		if(!inBox(b,r,c,x) && !inColumn(b,r,c,x) && !inRow(b,r,c,x))
+//			return true;
+//		else 
+//			return false;
+//	}
 }
