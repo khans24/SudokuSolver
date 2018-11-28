144eff13147e46082e176cc7e19a34d88ff4b603
khans24 <45401400+khans24@users.noreply.github.com>
2018-11-28 00:54:38 -0500

    Update BaseBoard.java

--- BaseBoard.java
+++ BaseBoard.java
@@ -2,6 +2,7 @@ package Solver;
 
 public class BaseBoard {	
 	public static void main(String[] args) {
+	//our 9x9 sudoku board, 0's indicate empty spots
 	int board[][] = new int[][]{
 	{5,8,0,1,7,0,3,0,0},
 	{0,0,9,0,0,8,0,1,0},
@@ -12,11 +13,13 @@ public class BaseBoard {
 	{3,0,0,0,0,0,5,0,0},
 	{0,6,0,5,0,0,2,0,0},
 	{0,0,7,0,8,3,0,6,9}};
-	//int column, row= 8;
-
+		
+	}
+	
 	//we checked inRow, inColumn, inBox methods. they work.
 	//next step is to manipulate the board (add another dummy board), then solve.
-	}
+	
+	//check if number x is in the row
 	private static boolean inRow(int board[][], int row, int x)
 	{
 		for(int i = 0;i <= 8; i++){
@@ -25,6 +28,7 @@ public class BaseBoard {
 		}
 		return false;
 	}
+	//check if number x is in the column
 	private static boolean inColumn(int board[][], int column, int x)
 	{
 		for(int i = 0; i <= 8; i ++){
@@ -33,6 +37,7 @@ public class BaseBoard {
 		}
 		return false;
 	}
+	//check if number x is in the 3x3 box
 	private static boolean inBox(int board[][], int row, int column, int x)
 	{
 		int row2 =(row / 3) *3;
@@ -45,10 +50,10 @@ public class BaseBoard {
 		}
 		return false;
 	}
-//	private boolean good(int b[][], int r, int c, int x){
-//		if(!inBox(b,r,c,x) && !inColumn(b,r,c,x) && !inRow(b,r,c,x))
-//			return true;
-//		else 
-//			return false;
-//	}
+	//check to see if all the Sudoku rules are met(column, row, and 3x3 box)
+	private static boolean good(int b[][], int r, int c, int x){
+		if(!inBox(b,r,c,x) && !inColumn(b,c,x) && !inRow(b,r,x))
+			return true;
+		return false;
+	}
 }
