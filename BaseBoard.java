5c3475fb51f4d55f92e03fb21e7963f487f3ac68
khans24 <45401400+khans24@users.noreply.github.com>
2018-11-28 00:56:23 -0500

    Update BaseBoard.java

--- BaseBoard.java
+++ BaseBoard.java
@@ -1,7 +1,6 @@
 package Solver;
 
 public class BaseBoard {	
-	public static void main(String[] args) {
 	//our 9x9 sudoku board, 0's indicate empty spots
 	int board[][] = new int[][]{
 	{5,8,0,1,7,0,3,0,0},
@@ -13,8 +12,6 @@ public class BaseBoard {
 	{3,0,0,0,0,0,5,0,0},
 	{0,6,0,5,0,0,2,0,0},
 	{0,0,7,0,8,3,0,6,9}};
-		
-	}
 	
 	//we checked inRow, inColumn, inBox methods. they work.
 	//next step is to manipulate the board (add another dummy board), then solve.
@@ -50,7 +47,7 @@ public class BaseBoard {
 		}
 		return false;
 	}
-	//check to see if all the Sudoku rules are met(column, row, and 3x3 box)
+	//check to see if all the Sudoku rules are met(number not in column, row, and 3x3 box)
 	private static boolean good(int b[][], int r, int c, int x){
 		if(!inBox(b,r,c,x) && !inColumn(b,c,x) && !inRow(b,r,x))
 			return true;
