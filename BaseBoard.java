b9210e61596f6c56875c3f332a33d9b3ed85882b
dottidee <38963734+dottidee@users.noreply.github.com>
2018-12-02 18:56:40 -0500

    Update BaseBoard.java

--- BaseBoard.java
+++ BaseBoard.java
@@ -3,20 +3,19 @@ package Solver;
 public class BaseBoard {	
 	//our 9x9 sudoku board, 0's indicate empty spots
 	
-	//i wanted to instantiate the array in the class and then put in the values in a constructor 
-	//with 2d arrays this doesn't really work however it seems to work just fine this way and manipulating
-	//the board is completely possible as long as our methods are public 
-	int board[][] = new int[][]{
-	{5,8,0,1,7,0,3,0,0},
-	{0,0,9,0,0,8,0,1,0},
-	{0,0,3,0,0,0,0,0,8},
-	{4,0,6,8,0,7,0,0,5},
-	{8,3,0,0,0,0,0,7,4},
-	{2,0,0,6,0,5,8,0,3},
-	{3,0,0,0,0,0,5,0,0},
-	{0,6,0,5,0,0,2,0,0},
-	{0,0,7,0,8,3,0,6,9}};
-	
+	//board is actually created in solve class
+	private int board[][] ;
+
+	//when baseboard object is created constructor creates the default board which is a 2d array.
+	//our 9x9 sudoku board, 0's indicate empty spots
+	public BaseBoard()
+	{
+		board = new int[9][9];
+	}
+	public BaseBoard(int x[][])
+	{
+		this.board = x;
+	}
 	//we checked inRow, inColumn, inBox methods. they work.
 	//next step is to manipulate the board (add another dummy board), then solve.
 	public int[][] getBoard()
